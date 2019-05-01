215f4f18a2e74528ff3eec6ab28736030813b816
Victoria <bernard.victoria22@gmail.com>
2019-05-01 11:27:34 -0500

    Add catch redirect

--- redirect.js
+++ redirect.js
@@ -1,17 +1,40 @@
-/**
- * Example Input
- *  */
+// Example Input
 const hostname = 'workers-tooling.cf'
 const externalHostname = 'victoriacf.tk'
 const REDIRECT_MAP = new Map([
-  ['/redirect1', 'https://' + externalHostname + '/redirect2'],
-  ['/redirect2', 'https://' + externalHostname + '/redirect3'],
-  ['/redirect3', 'https://' + externalHostname + '/redirect4'],
-  ['/redirect4', 'https://google.com'],
-]) /**  /redirect1 will 301 to /redirect2 etc. */
+    ['/redirect1', 'https://' + externalHostname + '/redirect2'],
+    ['/redirect2', 'https://' + externalHostname + '/redirect3'],
+    ['/redirect3', 'https://' + externalHostname + '/redirect4'],
+    ['/redirect4', 'https://google.com'],
+])
 const someURLToRedirectTo = 'https://www.google.com'
 const someURLThatSendsRedirects = 'http://victoriacf.tk/redirect1'
 const maxRedirects = 10
+
+/**
+ *  By default, fetch follows redirects. followRedirects
+ *  follows all the redirect so a clien't browser won't have
+ *  to
+ * @param {Request} req
+ */
+async function followRedirects(req) {
+    return await fetch(req)
+}
+
+/**
+ *  By default, fetch follows redirects. catchRedirects
+ *  stops the request at the first redirect
+ * @param {Request|string} req
+ */
+async function catchRedirects(req) {
+    const location = typeof req === 'string' ? req : req.url
+    const init =
+        typeof req === 'string'
+            ? { redirect: 'manual' }
+            : { ...req, redirect: 'manual' }
+
+    return await fetch(location, init)
+}
 
 /**
  * redirectResponse returns a redirect Response
@@ -19,26 +42,7 @@ const maxRedirects = 10
  * @param {number?=301|302} type permanent or temporary redirect
  */
 async function redirectResponse(url, type = 301) {
-  return Response.redirect(url, type) //await fetch(request)
-}
-
-/**
- * followRedirects returns a redirect Response based on url
- * so that a clients browser doesn't have to
- * @param {Request} url
- */
-async function followRedirects(url, type = 301) {
-  let i = 0
-  let location = url
-
-  do {
-    response = await fetch(location)
-    i++
-    let headers = new Headers(response.headers)
-    location = headers.get('Location')
-  } while (i < maxRedirects && response.redirected && location)
-
-  return response
+    return Response.redirect(url, type) //await fetch(request)
 }
 
 /**
@@ -47,33 +51,43 @@ async function followRedirects(url, type = 301) {
  * @param {Map<string, string>} redirectMap
  */
 async function bulkRedirects(request, redirectMap) {
-  let requestURL = new URL(request.url)
-  let path = requestURL.pathname
-  let location = redirectMap.get(path)
+    let requestURL = new URL(request.url)
+    let path = requestURL.pathname
+    let location = redirectMap.get(path)
 
-  // If location exists on map, return a redirect to it
-  if (location) {
-    return Response.redirect(location, 301)
-  }
-  // If not, return the original request
-  return fetch(request)
+    if (location) {
+        return Response.redirect(location, 301)
+    }
+    // If in map, return the original request
+    return fetch(request)
 }
 
 /**
  * Example of how redirect methods above can be used in an application
  *  */
 addEventListener('fetch', async event => {
-  const { request } = event
-  const { url, method } = request
+    const { request } = event
+    const { url, method } = request
 
-  if (url.match('/redirect[0-3]')) event.respondWith(bulkRedirects(request, REDIRECT_MAP))
-  if (url.endsWith('/follow')) event.respondWith(followRedirects(someURLThatSendsRedirects))
-  if (url.includes('/send')) event.respondWith(redirectResponse(someURLToRedirectTo))
-  if (url.includes('/device'))
-    event.respondWith(redirectOnDeviceType(event.request, 'mobile', MOBILE_REDIRECT_HOSTNAMES))
+    if (url.match('/redirect[0-3]'))
+        event.respondWith(bulkRedirects(request, REDIRECT_MAP))
+    if (url.endsWith('/follow'))
+        event.respondWith(followRedirects(someURLThatSendsRedirects))
+    if (url.endsWith('/not-follow'))
+        event.respondWith(catchRedirects(someURLThatSendsRedirects))
+    if (url.includes('/send'))
+        event.respondWith(redirectResponse(someURLToRedirectTo))
+    if (url.includes('/device'))
+        event.respondWith(
+            redirectOnDeviceType(
+                event.request,
+                'mobile',
+                MOBILE_REDIRECT_HOSTNAMES
+            )
+        )
 
-  // if doesn't match above return a dummy response for demonstration purposes
-  event.respondWith(new Response('some random response for ' + url))
+    // if doesn't match above return a dummy response for demonstration purposes
+    event.respondWith(new Response('some random response for ' + url))
 })
 
 /*************************** WIP ***************************/
@@ -83,25 +97,25 @@ addEventListener('fetch', async event => {
  * @param {Request} request
  */
 async function redirectOnDeviceType(request, deviceType, deviceHostnames) {
-  let requestURL = new URL(request.url)
-  let hostname = requestURL.hostname
-  let device = request.headers.get('CF-Device-Type')
+    let requestURL = new URL(request.url)
+    let hostname = requestURL.hostname
+    let device = request.headers.get('CF-Device-Type')
 
-  /**
-   *  Requires Enterprise "CF-Device-Type Header" zone setting or
-   *  Page Rule with "Cache By Device Type" setting applied.
-   */
-  if (device === deviceType) {
-    const deviceHostname = deviceHostnames[hostname]
-    if (deviceHostname) {
-      requestURL.hostname = deviceHostname
-      return Response.redirect(requestURL, 302)
+    /**
+     *  Requires Enterprise "CF-Device-Type Header" zone setting or
+     *  Page Rule with "Cache By Device Type" setting applied.
+     */
+    if (device === deviceType) {
+        const deviceHostname = deviceHostnames[hostname]
+        if (deviceHostname) {
+            requestURL.hostname = deviceHostname
+            return Response.redirect(requestURL, 302)
+        }
     }
-  }
-  return new Response('device was ', device) //await fetch(request)
+    return new Response('device was ', device) //await fetch(request)
 }
 const MOBILE_REDIRECT_HOSTNAMES = {
-  'a.example.com': 'a.m.' + hostname,
-  'www.example.com': 'm.' + hostname,
-  'example.com': 'm.' + hostname,
+    'a.example.com': 'a.m.' + hostname,
+    'www.example.com': 'm.' + hostname,
+    'example.com': 'm.' + hostname,
 }
